5193d9d52d90a348f93430704f85ecf66c1ddc37
kababukingdom <kababukingdom@gmail.com>
2023-01-13 14:19:57 -0800

    add functions

--- rot.c
+++ rot.c
@@ -9,18 +9,17 @@
   */
 void rotr(stack_t **stack, unsigned int line_number)
 {
-	stack_t *last;
+	stack_t *last = *stack;
 
 	(void)line_number;
 	if (*stack == NULL || (*stack)->next == NULL)
 		return;
 
-	last = *stack;
-	while (last->next != NULL) /* to get to the last element of the list */
+       	while (last->next != NULL) /* to get to the last element of the list */
 		last = last->next;
 	last->next = *stack;
 	(*stack)->prev = last;
-	*stack = (*stack)->next;
+	(*stack) = last;
 	(*stack)->prev->next = NULL;
 	(*stack)->prev = NULL;
 }
@@ -30,26 +29,24 @@ void rotr(stack_t **stack, unsigned int line_number)
   * @stack: double pointer to stack.
   * @line_number: number of lines.
   *
+  * The top element of the stack becomes the last one, and the second top,
+  * element of the stack becomes the first one.
+  * rotl never fails.
   * Return: void.
   */
 void rotl(stack_t **stack, unsigned int line_number)
 {
-	stack_t *b_last, *last;
+	stack_t *last = *stack, *b_last;
 
-	(void)line_number;
-
-	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	(void)(line_number);
+	if (*stack == NULL || (*stack)->next == NULL)
 		return;
-	last = *stack;
-	b_last = NULL;
+	b_last = (*stack)->next;
+	b_last->prev = NULL;
 	while (last->next != NULL)
-	{
-		b_last = last;
 		last = last->next;
-	}
-	(*stack)->prev = last;
 	last->next = *stack;
-	*stack = b_last->next;
-	b_last->next = NULL;
-
+	(*stack)->next = NULL;
+	(*stack)->prev = last;
+	(*stack) = b_last;
 }
