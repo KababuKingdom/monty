c99961f304a49a7a32e85171aa1ee0c5a2536373
kababukingdom <kababukingdom@gmail.com>
2023-01-14 12:54:27 -0800

    add funtions

--- rot.c
+++ rot.c
@@ -1,52 +1,68 @@
 #include "monty.h"
 
 /**
-  * rotr - rotates the stack to the bottom.
-  * @stack: double pointer to stack.
-  * @line_number: number of lins.
-  *
-  * Return: void.
-  */
+ * rotr - rotates the stack to the bottom
+ * @stack: pointer to the top of the stack
+ * @line_number: the line number in the file
+ *
+ * Return: void
+ */
 void rotr(stack_t **stack, unsigned int line_number)
 {
-	stack_t *last = *stack;
+    stack_t *temp, *last;
 
-	(void)line_number;
-	if (*stack == NULL || (*stack)->next == NULL)
-		return;
+    (void)line_number;
+    /* check if stack is empty or contains only one element */
+    if (!(*stack) || !(*stack)->next)
+        return;
 
-       	while (last->next != NULL) /* to get to the last element of the list */
-		last = last->next;
-	last->next = *stack;
-	(*stack)->prev = last;
-	(*stack) = last;
-	(*stack)->prev->next = NULL;
-	(*stack)->prev = NULL;
+    /* get the last stack */
+    last = *stack;
+    while (last->next)
+        last = last->next;
+
+    /* remove the last element */
+    temp = last;
+    last = last->prev;
+    last->next = NULL;
+    temp->prev = NULL;
+
+    /* make the last element the new head of the stack */
+    temp->next = *stack;
+    (*stack)->prev = temp;
+    *stack = temp;
 }
 
+#include "monty.h"
+
 /**
-  * rotl - rotates the stack to Top.
-  * @stack: double pointer to stack.
-  * @line_number: number of lines.
-  *
-  * The top element of the stack becomes the last one, and the second top,
-  * element of the stack becomes the first one.
-  * rotl never fails.
-  * Return: void.
-  */
+ * rotl - rotates the stack to the top
+ * @stack: pointer to the top of the stack
+ * @line_number: the line number in the file
+ *
+ * Return: void
+ */
 void rotl(stack_t **stack, unsigned int line_number)
 {
-	stack_t *last = *stack, *b_last;
+    stack_t *temp, *last;
 
-	(void)(line_number);
-	if (*stack == NULL || (*stack)->next == NULL)
-		return;
-	b_last = (*stack)->next;
-	b_last->prev = NULL;
-	while (last->next != NULL)
-		last = last->next;
-	last->next = *stack;
-	(*stack)->next = NULL;
-	(*stack)->prev = last;
-	(*stack) = b_last;
+    (void)line_number;
+    /* check if stack is empty or contains only one element */
+    if (!(*stack) || !(*stack)->next)
+        return;
+
+    /* get the last stack */
+    last = *stack;
+    while (tail->next)
+        last = last->next;
+
+    /* remove the head of the stack */
+    temp = *stack;
+    *stack = (*stack)->next;
+    (*stack)->prev = NULL;
+    temp->next = NULL;
+
+    /* make the head element the new tail of the stack */
+    temp->prev = last;
+    last->next = temp;
 }
